4745b5e9af44189e23a4c49be190e486de8dec58
Robert <92167234+17RPakulski@users.noreply.github.com>
2022-10-06 12:17:12 +0100

    Add files via upload

---
+++ revision exercise 1 q1 6 10 22.py
@@ -0,0 +1,10 @@
+user1 = float(input('Value 1: '))
+user2 = float(input('Value 2: '))
+
+summ = user1+user2
+dif = user1-user2
+prod = user1*user2
+
+print('Sum =',summ)
+print('Difference =',dif)
+print('Product =',prod)
