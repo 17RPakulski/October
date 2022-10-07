c12604340ac8de62a195f90a71595c1fe975198a
Robert <92167234+17RPakulski@users.noreply.github.com>
2022-10-07 13:07:00 +0100

    Add files via upload

---
+++ revision exercise 1 q5 6 10 22.py
@@ -0,0 +1,18 @@
+count = 0
+l = []
+l2 = []
+
+while count < 5:
+    u = int(input('Enter number : '))
+    count += 1
+    l.append(u)
+
+for i in l:
+    l2.append(i + 1)
+
+print('\n')
+print(l)
+print(l2)
+    
+
+    
