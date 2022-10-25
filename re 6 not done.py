0e118c9246b9d788654a7ab59c12e10d677443f5
Robert <92167234+17RPakulski@users.noreply.github.com>
2022-10-25 10:21:16 +0100

    Add files via upload

---
+++ re 6 not done.py
@@ -0,0 +1,13 @@
+num = int(input('Enter: '))
+reverse = 0
+lastDigit = 0
+
+while num > 0:
+    lastDigit = num % 10
+    reverse *= 10
+    reverse += lastDigit
+    
+                
+    
+    
+    
